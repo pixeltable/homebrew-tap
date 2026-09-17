da0951674e32e082d547244f10b43f231a02e476
Pierre Brunelle <pierre@noteable.io>
2026-09-16 21:21:30 -0700

    fix(formula): invoke python3.12 -m pip --python for venvs created without pip

--- Formula/pxt.rb
+++ Formula/pxt.rb
@@ -20,7 +20,9 @@ class Pxt < Formula
 
   def install
     virtualenv_create(libexec, "python3.12")
-    system libexec/"bin/pip", "install",
+    system "python3.12", "-m", "pip",
+           "--python=#{libexec}/bin/python",
+           "install",
            "--no-warn-script-location",
            "--prefer-binary",
            "#{cached_download}[serve]"
